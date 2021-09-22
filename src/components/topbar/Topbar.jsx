1e19244fa3aecc40ab7162af63926015cae53e42
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2021-09-21 22:24:46 -0500

    adding ID for components
    assigned IDs to components in jsx files

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,10 +5,10 @@ export default function Topbar() {
         <div className="topbar">
             <div className="wrapper">
                 <div className="left">
-                    
+                    <a href="#intro">asdasd</a>
                 </div>
                 <div className="right">
-                    
+
                 </div>
             </div>
         </div>
