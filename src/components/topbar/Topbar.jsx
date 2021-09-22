9a8a853dee6310c9c3460184a8365a9ee08e18f2
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2021-09-22 09:59:56 -0500

    adding icons
    contact info added with icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons";
 
 export default function Topbar() {
     return (
@@ -6,6 +7,14 @@ export default function Topbar() {
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">Tech Nerd</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>444 555 6666</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>Asadfgad@kdjgsad.com</span>
+                    </div>
                 </div>
                 <div className="right"></div>
             </div>
