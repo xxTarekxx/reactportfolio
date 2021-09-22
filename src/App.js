d4ca7e4f9807c9b6ff137c937667a3044c52de95
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2021-09-21 22:22:03 -0500

    laying more components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import Topbar from "./components/topbar/Topbar"
+import Intro from "./components/intro/Intro"
+import Portfolio from "./components/portfolio/Portfolio"
+import Contact from "./components/contact/Contact"
+import "./app.scss"
 
 function App() {
   return (
-    <div className="App">
-      top
-      sec -intro -portfolio -contact
+    <div className="app">
+      <Topbar/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Contact/>
+      </div>
     </div>
   );
 }
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,16 @@
+import "./topbar.scss"
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    
+                </div>
+                <div className="right">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
