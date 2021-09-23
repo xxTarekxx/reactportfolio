0f9747dc3fb852be33c28b809f275669a9147e0d
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2021-09-23 08:59:15 -0500

    changed location of project
    moved location of project on machine readjusting files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import Topbar from "./components/topbar/Topbar"
-import Intro from "./components/intro/Intro"
-import Portfolio from "./components/portfolio/Portfolio"
-import Contact from "./components/contact/Contact"
-import "./app.scss"
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import "./app.scss";
+import { useState } from "react";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(true);
   return (
     <div className="app">
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
-        <Intro/>
-        <Portfolio/>
-        <Contact/>
+        <Intro />
+        <Portfolio />
+        <Contact />
       </div>
     </div>
   );
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,22 +1,28 @@
 import "./topbar.scss"
-import {Person, Mail} from "@material-ui/icons";
+import { Person, Mail } from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className="topbar">
-            <div className="wrapper">
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper"> {/*wrapping navbar components*/}
                 <div className="left">
                     <a href="#intro" className="logo">Tech Nerd</a>
                     <div className="itemContainer">
-                        <Person className="icon"/>
+                        <Person className="icon" />
                         <span>444 555 6666</span>
                     </div>
                     <div className="itemContainer">
-                        <Mail className="icon"/>
+                        <Mail className="icon" />
                         <span>Asadfgad@kdjgsad.com</span>
                     </div>
                 </div>
-                <div className="right"></div>
+                <div className="right">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
             </div>
         </div>
     )
