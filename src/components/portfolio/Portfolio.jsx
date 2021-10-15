34f39f1060ebcc8a52467c2485c30d931dbe9659
Tarek <tarektawfik90@outlook.com>
2021-10-14 21:36:59 -0500

    changed intro photo
    Changed the intro photo

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,17 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            Portfolion Page
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>Web App</li>
+                <li>Contribution</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    Weather App
+                </div>
+            </div>
         </div>
     )
 }
