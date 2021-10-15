230a6f54364aa8b0b51a30e70942bff65f759b65
Tarek <tarektawfik90@outlook.com>
2021-10-14 21:09:44 -0500

    Replacing files

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,20 +1,37 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
-    return (
-        <div className="intro" id="intro">
-            <div className="left">
-                <div className="imgContainer" id="imgContainer">
-                    <img src="assets/man.png" alt="" />
-                </div>
-            </div>
-            <div className="right">
-                <div className="wrapper">
-                    <h2>Hello, I'm </h2>
-                    <h1>Tarek Ismael</h1>
-                    <h3>Front End Developer</h3>
-                </div>
-            </div>
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,
+      backSpeed: 50,
+      showCursor: true,
+      strings: ["Developer", "ReactJS", "Angular", "Bootstrap", "VueJs"],
+    });
+  }, []);
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer" id="imgContainer">
+          <img src="assets/man.png" alt="" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hello, I'm </h2>
+          <h1>Tarek Ismael</h1>
+          <h3>
+            Front End <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
